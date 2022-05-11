76a88066c387db67587ee047e3589046b8886c1f
Dell <jenishdobariya2003@gmail.com>
2022-05-11 19:52:39 +0530

    1.1

--- app/src/main/java/com/example/fruit_app/Flash_screen.java
+++ app/src/main/java/com/example/fruit_app/Flash_screen.java
@@ -12,12 +12,16 @@ public class Flash_screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flash_screen);
+
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+
                 Intent intent =new Intent(Flash_screen.this,MainActivity.class);
                 startActivity(intent);
                 finish();
+
             }
         }, 1000);
 
